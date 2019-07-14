9868aeba6475bad548c4ff476c257739485f1552
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-07-14 09:28:37 -0400

    Define list_display fields

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,11 @@ from . import models
 
 
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'title',
+        'created',
+        'updated',
+    )
 
 
 admin.site.register(models.Post, PostAdmin)
