c6291f6cf9b0b2dc7f19520bec238f68a2d5ccf8
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-07-14 09:35:21 -0400

    Set default ordering to -created

--- blog/models.py
+++ blog/models.py
@@ -10,5 +10,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)  # Sets on create
     updated = models.DateTimeField(auto_now=True)  # Updates on each save
 
+    class Meta:
+        ordering = ['-created']
+
     def __str__(self):
         return self.title
