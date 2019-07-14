16b6d4cbfd8019f3030bf160204ead6f7deeeb46
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-07-14 08:58:03 -0400

    Define the __str__ method of the Post model

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,6 @@ class Post(models.Model):
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)  # Sets on create
     updated = models.DateTimeField(auto_now=True)  # Updates on each save
+
+    def __str__(self):
+        return self.title
