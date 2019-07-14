cab6a52d06e4b4e3223cddafbe24af90b47580b6
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-07-14 11:44:22 -0400

    Add status field to Post

--- blog/models.py
+++ blog/models.py
@@ -6,12 +6,25 @@ class Post(models.Model):
     """
     Represents a blog post
     """
+    DRAFT = 'draft'
+    PUBLISHED = 'published'
+    STATUS_CHOICES = [
+        (DRAFT, 'Draft'),
+        (PUBLISHED, 'Published')
+    ]
+
     title = models.CharField(max_length=255)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,  # The Django auth user model
         on_delete=models.PROTECT,  # Prevent posts from being deleted
         related_name='blog_posts',
         null=True,
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default=DRAFT,
+        help_text='Set to "published" to make this post publicly visible',
     )
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)  # Sets on create
