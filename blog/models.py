317bc7f10bce0a4be1ccea5287a6125e3aa51f0f
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-07-14 08:16:55 -0400

    Create a Post model & migration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    Represents a blog post
+    """
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)  # Sets on create
+    updated = models.DateTimeField(auto_now=True)  # Updates on each save
