5df65f5c4d2290c880333a10fc8f258dfbb7404c
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-07-14 10:46:21 -0400

    Add an author ForeignKey to the post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -6,6 +7,12 @@ class Post(models.Model):
     Represents a blog post
     """
     title = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,  # The Django auth user model
+        on_delete=models.PROTECT,  # Prevent posts from being deleted
+        related_name='blog_posts',
+        null=True,
+    )
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)  # Sets on create
     updated = models.DateTimeField(auto_now=True)  # Updates on each save
