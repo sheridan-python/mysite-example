3b30f3b3dba9ea38da7da08167b225391aae80e9
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-08-04 14:52:37 -0400

    Render latest blog posts on the home page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from . import models
 
 
 def home(request):
     """
     The Blog homepage
     """
-    return render(request, 'blog/home.html')
+    latest_posts = models.Post.objects.published().order_by('-published')[:3]
+    context = {'latest_posts': latest_posts}
+    return render(request, 'blog/home.html', context)
