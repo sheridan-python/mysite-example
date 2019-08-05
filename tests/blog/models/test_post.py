cf7a96125d268c9089e81a9cfab3db8dc0145a97
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-08-05 14:18:22 -0400

    Write test for get_authors queryset method

--- tests/blog/models/test_post.py
+++ tests/blog/models/test_post.py
@@ -50,3 +50,14 @@ def test_publish_sets_published_to_current_datetime():
         2030, 6, 1, 12,
         tzinfo=dt.timezone.utc
     )
+
+
+def test_get_authors_returns_users_who_have_authored_a_post(django_user_model):
+    # Create a user
+    author = mommy.make(django_user_model)
+    # Create a post that is authored by the user
+    mommy.make('blog.Post', author=author)
+    # Create another user – but this one won't have any posts
+    mommy.make(django_user_model)
+
+    assert list(Post.objects.get_authors()) == [author]
