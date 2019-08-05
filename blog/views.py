cd716a0caf54751339024ff0f6c81b1aa23f523e
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-08-05 14:58:40 -0400

    Call get_authors query in the view

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,11 @@ def home(request):
     The Blog homepage
     """
     latest_posts = models.Post.objects.published().order_by('-published')[:3]
-    context = {'latest_posts': latest_posts}
+    authors = models.Post.objects.get_authors()
+
+    context = {
+        'authors': authors,
+        'latest_posts': latest_posts
+    }
+
     return render(request, 'blog/home.html', context)
