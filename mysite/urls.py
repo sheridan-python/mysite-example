bdde7fa17ebff00ce6270a6a840ea9aaabb8f235
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-08-06 15:53:33 -0400

    Link navigation to post list

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,7 +6,7 @@ from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.AboutView.as_view(), name='about'),
-    path('posts/', views.PostListView.as_view(), name='posts'),
+    path('posts/', views.PostListView.as_view(), name='post-list'),
     path('terms/', views.terms_and_conditions, name='terms-and-conditions'),
     path('', views.HomeView.as_view(), name='home'),
 ]
