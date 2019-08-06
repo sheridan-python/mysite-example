be9e251abff6655e6bd3f957bea2aea073cf71ac
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-08-06 10:55:39 -0400

    Delete duplicate code from home & about views

--- blog/views.py
+++ blog/views.py
@@ -21,30 +21,13 @@ class HomeView(ContextMixin, TemplateView):
     def get_context_data(self, **kwargs):
         # Get the parent context
         context = super().get_context_data(**kwargs)
-
         latest_posts = models.Post.objects.published() \
             .order_by('-published')[:3]
 
-        authors = models.Post.objects.published() \
-            .get_authors() \
-            .order_by('first_name')
-
-        # Update the context with our context variables
-        context.update({
-            'authors': authors,
-            'latest_posts': latest_posts
-        })
+        context.update({'latest_posts': latest_posts})
 
         return context
 
 
 class AboutView(ContextMixin, TemplateView):
     template_name = 'blog/about.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['authors'] = models.Post.objects.published() \
-            .get_authors() \
-            .order_by('first_name')
-
-        return context
