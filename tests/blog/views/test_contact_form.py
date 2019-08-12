d60076dba00bb170f10633e72ae1d6f1831cb2ef
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-08-12 11:12:52 -0400

    Add invalid submission test

--- tests/blog/views/test_contact_form.py
+++ tests/blog/views/test_contact_form.py
@@ -32,3 +32,10 @@ def test_post_contact_form_saves_data(client):
     assert obj.last_name == data['last_name']
     assert obj.email == data['email']
     assert obj.message == data['message']
+
+
+def test_invalid_submission(client):
+    data = {'fizz': 'buzz'}
+    response = client.post('/contact/', data)
+    assert response.status_code != 302
+    assert Contact.objects.exists() is False
